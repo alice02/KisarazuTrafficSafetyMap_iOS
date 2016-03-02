78f7c542a138176da0e024eb5242707e84e38580
Kouta ASAI <scene.4913@gmail.com>
2016-03-02 16:49:24 +0900

    交通事故マップ用のViewControllerを作成

---
+++ KisarazuTrafficSafetyMap/TrafficAccidentMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TrafficAccidentMapViewController.swift
+//  KisarazuTrafficSafetyMap
+//
+//  Created by Kouta on 2016/03/02.
+//  Copyright © 2016年 Kouta. All rights reserved.
+//
+
+import UIKit
+
+class TrafficAccidentMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
