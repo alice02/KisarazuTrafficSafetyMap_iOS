7cc81e161ff449fd862f7d5b131d9c3ff6c75377
Kouta ASAI <scene.4913@gmail.com>
2016-03-02 16:27:26 +0900

    Initial Commit

---
+++ KisarazuTrafficSafetyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KisarazuTrafficSafetyMap
+//
+//  Created by Kouta on 2016/03/02.
+//  Copyright © 2016年 Kouta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
