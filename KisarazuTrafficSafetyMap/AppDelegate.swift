263753d351ee3481d13701c89c2d6e8cf61e40b0
Kouta ASAI <scene.4913@gmail.com>
2016-03-04 16:52:34 +0900

    設定画面にコンテンツを追加

--- KisarazuTrafficSafetyMap/AppDelegate.swift
+++ KisarazuTrafficSafetyMap/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    // スリープの抑止
+    var restrainSleep: Bool = false
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
--- KisarazuTrafficSafetyMap/ConfigViewController.swift
+++ KisarazuTrafficSafetyMap/ConfigViewController.swift
@@ -10,20 +10,43 @@ import UIKit
 
 class ConfigViewController: UITableViewController {
 
+    
+    
     @IBAction func backButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
-        print("back")
     }
+
+
+    let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    @IBOutlet weak var sleepSwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let restrainSleep = appDelegate.restrainSleep
+        
+        sleepSwitch.on = restrainSleep
+        
+        sleepSwitch.addTarget(self, action: "onChangeSleepSwitch:", forControlEvents: .ValueChanged)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func onChangeSleepSwitch(sender: UISwitch) {
+        if sender.on {
+            UIApplication.sharedApplication().idleTimerDisabled = true
+            appDelegate.restrainSleep = true
+        }
+        else {
+            UIApplication.sharedApplication().idleTimerDisabled = false
+            appDelegate.restrainSleep = false
+        }
+    }
+ 
 }
 
